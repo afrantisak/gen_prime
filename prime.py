eae323de79c0d3811c5967c8066105a554c3f354
Aaron Frantisak <afrantisak@gmail.com>
2016-01-05 18:33:12 -0600

    refactor for readability

--- prime.py
+++ prime.py
@@ -2,9 +2,21 @@ import math
 
 def generator():
     yield 2
-    for num in xrange(3,100001,2):
+    num = 3
+    while True:
         if all(num % i != 0 for i in xrange(2, int(math.sqrt(num)) + 1)):
             yield num
+        num += 2
 
-for num in generator():
-    print num            
+def primes_less_than(max):
+    gen = generator()
+    num = gen.next()
+    while num < max:
+        print num
+        num = gen.next()
+    
+def main():
+    return primes_less_than(100000)
+
+if __name__ == "__main__":
+    main()
