fbfe2c888a28e6d68975186cdbfaf8a0b5d0f234
Aaron Frantisak <afrantisak@gmail.com>
2016-01-08 07:59:12 -0600

    dbl cleanup

--- graph.py
+++ graph.py
@@ -1,7 +1,9 @@
 import sys
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import prime
+import sets
 
 def get_deltas(iterable, init):
     x = []
@@ -20,28 +22,38 @@ def plot_prime_deltas(maximum):
     x, y = get_prime_deltas(maximum)
     plot_2d(x, y)
 
-def plot_prime_dbl_deltas(maximum, n):
+def plot_prime_dbl_deltas(maximum):
     x, y = get_prime_deltas(maximum)
+    deltas = sets.Set(y)
+    colors = matplotlib.cm.rainbow(np.linspace(0, 1, len(deltas)))
     xy = zip(x, y)
-    xyf = [p for p in xy if p[1] == n]
-    xf, yf = zip(*xyf)
-    x2, y2 = get_deltas(xf, 0)
-    plot_2d(x2, y2)
+    x2 = []
+    y2 = []
+    z2 = []
+    for index, n in enumerate(deltas):
+        xyf = [p for p in xy if p[1] == n]
+        if not xyf: continue
+        xf, yf = zip(*xyf)
+        xn, yn = get_deltas(xf, 0)
+        x2 += [n] * len(xn)
+        y2 += yn
+        z2 += [colors[index]] * len(xn)
+    plot_2d(x2, y2, z2)
 
-def plot_2d(x, y):
+def plot_2d(x, y, c=None):
     max_x = max(x)
     max_y = max(y)
-    edg_x = max_x * 0.1
-    edg_y = max_y * 0.1
+    edg_x = max_x * 0.05
+    edg_y = max_y * 0.05
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-    ax.scatter(x, y)
+    ax.scatter(x, y, c=c)
     ax.axis([-edg_x, max_x + edg_x, -edg_y, max_y + edg_y])
     plt.show()
 
 def main():
     num = int(sys.argv[1]) if len(sys.argv) > 1 else 100
-    plot_prime_dbl_deltas(num, 6)
+    plot_prime_dbl_deltas(num)
     #plot_prime_deltas(num)
     return 0
 
