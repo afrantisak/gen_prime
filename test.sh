f0d6f683e8ae823f4a68e68a37462657cc010541
Aaron Frantisak <afrantisak@gmail.com>
2016-01-14 17:31:18 -0600

    more testing

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-python prime.py 100000 | diff test.1e5.out -
-
+python prime.py 100000  | diff test_data/prime.1e5.out -
+#python prime.py 1000000 | diff test_data/prime.1e6.out -
+python prime_deltas.py 400 | diff test_data/prime_deltas.400.out -
